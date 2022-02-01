d94699eb44e126951c8ce0978420b373da1ab732
Vityamin-N <heartless41196@gmail.com>
2022-02-01 08:36:53 +0200

    Completed task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+const formSubmit = (event) => {
+    event.preventDefault();
+    const {email, password} = event.currentTarget;
+    if(email.value || password.value){
+        const formData = {};
+        for(const {name, value} of event.currentTarget.elements){
+            if(name){
+                formData[name] = value;
+            }
+        }
+        console.log(formData);
+        event.currentTarget.reset();
+    } else alert("Все поля должны быть заполнены");
+};
+form.addEventListener("submit", formSubmit);
