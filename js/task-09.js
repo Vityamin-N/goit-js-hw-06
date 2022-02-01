89a90f7defe292311571951aa0d43cf59957a057
Vityamin-N <heartless41196@gmail.com>
2022-02-01 08:44:34 +0200

    Completed task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const items = {
+  button: document.querySelector(".change-color"),
+  span: document.querySelector(".color"),
+  body: document.body,
+};
+
+const colorChange = () => {
+  const backgroundColorChange = getRandomHexColor();
+  items.span.textContent = backgroundColorChange;
+  items.body.style.backgroundColor = backgroundColorChange;
+};
+items.button.addEventListener("click", colorChange);
