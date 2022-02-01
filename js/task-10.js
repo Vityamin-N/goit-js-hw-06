bd3ab2a426116df7c92f0a388aadebf49731339a
Vityamin-N <heartless41196@gmail.com>
2022-02-01 09:23:34 +0200

    Completed task 10(optional)

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const items = {
+  control: document.querySelector("#controls input"),
+  createButton: document.querySelector("[data-create]"),
+  destroyButton: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+
+const createBoxes = (amount) => {
+  let size = 30;
+  const boxArray = [];
+  for(let i = 0; i < amount; i += 1){
+    const boxElement = document.createElement("div");
+    boxElement.style.width = `${size}px`;
+    boxElement.style.height = `${size}px`;
+    boxElement.style.backgroundColor = getRandomHexColor();
+    size += 10;
+    boxArray.push(boxElement);
+  }
+  items.boxes.append(...boxArray);
+};
+
+const destroyBoxes = () => items.boxes.innerHTML = "";
+const clickCreate = () => {
+  if(items.control.value) {
+    createBoxes(items.control.value);
+    items.control.value = "";
+  }
+};
+
+items.createButton.addEventListener("click", clickCreate);
+items.destroyButton.addEventListener("click", destroyBoxes);
